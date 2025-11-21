8a92058679a88a36a183bf56b35a929f36294f48
kinsluv2025-ops <kinsluv2025@gmail.com>
2025-11-21 18:23:56 +0100

    Delete kinsluv_full_code directory
    deleted

--- kinsluv_full_code/lib/src/services/mock_data.dart
+++
@@ -1,7 +0,0 @@
-// Simple mock helpers used by scaffold
-class MockData {
-  static List<Map<String,dynamic>> sampleGifts() => [
-    {'id':'g1','name':'Rose','price':10,'lottieUrl':''},
-    {'id':'g2','name':'Diamond','price':50,'lottieUrl':''},
-  ];
-}
